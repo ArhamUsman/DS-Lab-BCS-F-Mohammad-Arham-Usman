49b81517150ec6af7ea517b11ac5c42a0228ff0e
Mohammad Arham Usman <144592851+ArhamUsman@users.noreply.github.com>
2025-08-23 16:55:31 +0500

    Update Q1.cpp

--- Lab1/Q1.cpp
+++ Lab1/Q1.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+class BankAccount{
+private:
+    int balance;    
+public:
+    BankAccount(int balance=0){
+        this->balance=balance;
+    }
+    BankAccount(const BankAccount& b1){
+        balance=b1.balance;
+    }
+    bool withdraw(int amount){
+        if (balance>=amount){
+            balance-=amount;
+            return true;
+        }
+        return false;
+    }
+    void display(){
+        cout<<"Balance: "<<balance<<endl;
+    }
+};
+int main() {
+    BankAccount b1, b2(1000), b3(b2);
+    cout<<"Account 1:\n";
+    b1.display();
+    cout<<"Account 2:\n";
+    b2.display();
+    b3.withdraw(200);
+    cout<<"Account 3 (after withdraw):\n";
+    b3.display();
+    cout<<"Account 2 (after withdraw from account 3):\n";
+    b2.display();
+    
+    return 0;
+}
