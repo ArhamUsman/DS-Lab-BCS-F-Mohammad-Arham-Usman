6b5746fd3c9b0e0fece651bf7914099e7003b428
Mohammad Arham Usman <144592851+ArhamUsman@users.noreply.github.com>
2025-11-30 15:35:23 +0500

    Update 1.cpp

--- Lab 12/1.cpp
+++ Lab 12/1.cpp
@@ -1,28 +1,3 @@
-Conversation opened. 1 unread message.
-
-Skip to content
-Using NUCES-FAST Mail with screen readers
-
-1 of 2,866
-Ds
-External
-Inbox
-
-M. Arham Usman
-Attachments
-3:33 PM (0 minutes ago)
-to me
-
-
-
-Kind Regards,
-M. Arham Usman
-
- 3 Attachments
-  •  Scanned by Gmail
-
-
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -64,5 +39,3 @@ int main(){
         cout<<s<<endl;
     }
 }
-1.cpp
-Displaying 1.cpp.
